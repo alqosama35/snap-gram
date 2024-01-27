f5b43e6a1b951f142637c5939d70669d550c4f7e
alqosama35 <alqosama35@gmail.com>
2024-01-27 15:37:44 +0200

    28_start to design signup Form

---
+++ src/App.tsx
@@ -0,0 +1,29 @@
+
+import AuthLayout from './_auth/AuthLayout';
+import SigninForm from './_auth/forms/SigninForm';
+import SignupForm from './_auth/forms/SignupForm';
+import RootLayout from './_root/RootLayout';
+import {Home} from './_root/pages';
+import './globals.css';
+import { Route, Routes } from 'react-router-dom';
+
+const App = () => {
+    
+  return (
+    <main className="flex h-screen">
+        <Routes>
+            //public Routes
+            <Route element={<AuthLayout />}>
+                <Route path='/signin' element={<SigninForm />} />
+                <Route path='/signup' element={<SignupForm />} />
+            </Route>
+            //private Routes
+            <Route element={<RootLayout />}>
+                <Route index element={<Home />} />
+            </Route>
+        </Routes>
+    </main>
+  )
+}
+
+export default App
---
+++ src/_auth/AuthLayout.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import SignupForm from './forms/SignupForm'
+
+const AuthLayout = () => {
+  return (
+    <div>
+      <SignupForm/>
+    </div>
+  )
+}
+
+export default AuthLayout
