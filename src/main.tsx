fde151617be1478d18cbb28e1602c47fa931d90f
alqosama35 <alqosama35@gmail.com>
2024-02-07 20:27:59 +0200

    page rendered successfuly after solving the error

--- src/main.tsx
+++ src/main.tsx
@@ -4,15 +4,17 @@ import App from "./App"
 import { BrowserRouter } from "react-router-dom"// to make app.tsx the router
 import AuthProvider from "./context/AuthContext"
 import { QueryProvider } from "./lib/react-query/QueryProviders"
+import React from "react"
 
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-    <BrowserRouter>
-    <QueryProvider>
-       <AuthProvider>
-         <App />
-        </AuthProvider> 
-    </QueryProvider>
-        
-    </BrowserRouter>
-)
+ReactDOM.createRoot(document.getElementById("root")!).render(
+    <React.StrictMode>
+      <BrowserRouter>
+        <QueryProvider>
+          <AuthProvider>
+            <App />
+          </AuthProvider>
+        </QueryProvider>
+      </BrowserRouter>
+    </React.StrictMode>
+  );
